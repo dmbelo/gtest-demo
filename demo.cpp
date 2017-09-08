7dd255772f4ae0b85bbde3c206fa9f8c939db6ab
dmbelo <david.belo@mclaren.com>
2017-09-08 16:42:50 -0700

    Basic example

---
+++ demo.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <gtest/gtest.h>
+
+struct BankAccount
+{
+
+    int balance = 0;
+
+    BankAccount()
+    {
+
+    }
+
+    explicit BankAccount(const int balance) : balance{balance}
+    {
+
+    }
+
+};
+
+TEST(AccountTest, BankAccountStartsEmpty)
+{
+    BankAccount account;
+    EXPECT_EQ(0, account.balance);
+}
+
+int main(int argc, char* argv[])
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+    return 0;
+}
