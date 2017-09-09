6ce9f2577a8c4d7f62a918c947868a184083f033
dmbelo <david.belo@mclaren.com>
2017-09-08 18:10:36 -0700

    Test fixtures included

--- demo.cpp
+++ demo.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <gtest/gtest.h>
 
-struct BankAccount
+class BankAccount
 {
+
+public:
 
     int balance = 0;
 
@@ -16,17 +18,52 @@ struct BankAccount
 
     }
 
+    void deposit(int amount)
+    {
+        balance += amount;
+    }
+
+    int withdraw(int amount)
+    {
+        if (amount <= balance)
+        {
+            balance -= amount;
+            return 1;
+        }
+        return 0;
+    }
+
 };
 
-TEST(AccountTest, BankAccountStartsEmpty)
+struct BankAccountTest : testing::Test
 {
-    BankAccount account;
-    EXPECT_EQ(0, account.balance);
+
+    BankAccount* account;
+
+    BankAccountTest()
+    {
+        account = new BankAccount;
+    }
+
+    virtual ~BankAccountTest()
+    {
+        delete account;
+    }
+};
+
+TEST_F(BankAccountTest, BankAccountStartsEmpty)
+{
+    EXPECT_EQ(0, account->balance);
+}
+
+TEST_F(BankAccountTest, CanDepositMoney)
+{
+    account->deposit(100);
+    EXPECT_EQ(100, account->balance);
 }
 
 int main(int argc, char* argv[])
 {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
-    return 0;
 }
